3d61dbbae72cce31afa93515f7d306e20a76d5ad
Edward <snegovikzimniy@gmail.com>
2026-04-07 12:43:18 +0300

    deploy

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,15 +1,15 @@
 import { Header } from "@widgets/Header/Header";
 import "@styles/nullstyle.css";
 import "@styles/global.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AppRouter } from "@app/router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App/App.js";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Missing root Element");
